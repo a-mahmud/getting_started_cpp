d5dc42b35645a5829337d3ff4dab3c4ca89e32e2
a-mahmud <52372951+a-mahmud@users.noreply.github.com>
2019-06-30 19:22:19 +0600

    Add files via upload

---
+++ cpp_getting_started/main.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    // it will once we set up the correct path for mingw
+    printf("Hello Bru!");
+    printf("Does this work?");
+    printf("oww yeah"); 
+    printf("but how to run code?");
+    int someVar = 0;
+    scanf("enter something %d", &someVar);
+
+    // for that you need to go through that link
+    // there it is specifed how you will run code
+    // can you open a cpp properties json file?
+    // and update the path there
+    return 0;
+
+}
